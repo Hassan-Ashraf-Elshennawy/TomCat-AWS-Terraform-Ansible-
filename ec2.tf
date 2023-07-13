639d8257ddbc9732470f48d75c586f6bb3d4c4be
hussin <hassan@hussin-Inspiron-5520>
2023-07-13 20:35:42 +0300

    using ansible playbook to configure the server which proisined by terraform cosde as tomcat webserver

--- ec2.tf
+++ ec2.tf
@@ -9,13 +9,13 @@ resource "aws_instance" "web_server" {
     Name = "web_server"
   }
     # Store private key :  Generate and save private key(aws_keys_pairs.pem) in current directory 
-    #
+    #fetch the IP address from a newly provisioned server and store in inventory file
   provisioner "local-exec" {   
      interpreter = ["/bin/bash" ,"-c"]
     command = <<-EOT
       echo '${tls_private_key.terrafrom_generated_private_key.private_key_pem}' > aws_keys_pairs.pem
       chmod 400 aws_keys_pairs.pem
-      echo '${aws_instance.web_server.public_ip}' > inventory
+      echo '${aws_instance.web_server.public_ip}' > ./ansible/inventory
     EOT
    
 
